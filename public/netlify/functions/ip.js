2dd83688cf088e592a80af43a05e8fb3b6ff1320
Eric Bouchard <github@ericbouchard.com>
2022-01-12 23:28:19 -0500

    Return IP

--- public/netlify/functions/ip.js
+++ public/netlify/functions/ip.js
@@ -1,6 +1,13 @@
 exports.handler = async function(event, context) {
+
+    let client_ip = event.headers['cf-connecting-ip'];
+
+    if (!client_ip) {
+        client_ip = event.headers['client-ip']
+    }
+
     return {
         statusCode: 200,
-        body: JSON.stringify(event.headers)
+        body: client_ip
     };
 }
